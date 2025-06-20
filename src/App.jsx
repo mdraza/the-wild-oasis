7d345c1cb3ea90f181a034ecf19013c338e5a10f
mdraza <raza.uidev@gmail.com>
2025-06-20 15:30:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  padding: 20px;
+  background-color: orangered;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check In and Out</Heading>
+        <Button onClick={() => alert("Check In")}>Check In</Button>
+        <Button onClick={() => alert("Check Out")}>Check Out</Button>
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
